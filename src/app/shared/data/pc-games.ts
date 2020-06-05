724e470cb4bd69bcabcd2c6b74c1271754f84bae
alexein777 <a.mladenovic96@gmail.com>
2020-06-05 23:12:24 +0200

    more pc-games, better grid

--- src/app/shared/data/pc-games.ts
+++ src/app/shared/data/pc-games.ts
@@ -10,6 +10,13 @@ export const PC_GAMES: Game[] = [
     imageSrc: '../../../assets/images/games/pc/dota2.png'
   },
   {
+    name: 'League of Legends',
+    console: 'pc',
+    type: 'MOBA',
+    esport: true,
+    imageSrc: ''
+  },
+  {
     name: 'World of Warcraft',
     expansion: 'Shadowlands',
     console: 'pc',
@@ -19,7 +26,15 @@ export const PC_GAMES: Game[] = [
     imageSrc: '../../../assets/images/games/pc/wow-shadowlands.png'
   },
   {
-    name: 'The Elder Scrolls: Skyrim',
+    name: 'The Elder Scrolls IV: Oblivion',
+    console: 'pc',
+    type: 'RPG',
+    rating: 4.8,
+    esport: false,
+    imageSrc: '../../../assets/images/games/pc/skyrim.png'
+  },
+  {
+    name: 'The Elder Scrolls V: Skyrim',
     console: 'pc',
     type: 'RPG',
     rating: 4.8,
@@ -35,12 +50,38 @@ export const PC_GAMES: Game[] = [
     imageSrc: '../../../assets/images/games/pc/tes-legends.png'
   },
   {
+    name: 'Counter Strike 1.6',
+    console: 'pc',
+    type: 'First-person shooter',
+    rating: 4.8,
+    esport: true,
+    imageSrc: '../../../assets/images/games/pc/cs-go.jpg'
+  },
+  {
     name: 'Counter Strike: Global Offensive',
     console: 'pc',
     type: 'First-person shooter',
     rating: 4.8,
     esport: true,
     imageSrc: '../../../assets/images/games/pc/cs-go.jpg'
+  },
+  {
+    name: 'Assassin\'s Creed',
+    expansion: 'French?',
+    console: 'pc',
+    type: 'RPG',
+    rating: 4.0,
+    esport: false,
+    imageSrc: '../../../assets/images/games/pc/ac-valhalla.png'
+  },
+  {
+    name: 'Assassin\'s Creed',
+    expansion: 'Sparta?',
+    console: 'pc',
+    type: 'RPG',
+    rating: 4.0,
+    esport: false,
+    imageSrc: '../../../assets/images/games/pc/ac-valhalla.png'
   },
   {
     name: 'Assassin\'s Creed',
@@ -59,6 +100,68 @@ export const PC_GAMES: Game[] = [
     rating: 3.9,
     esport: false,
     imageSrc: '../../../assets/images/games/pc/generals-zh.jpg'
+  },
+  {
+    name: 'Starcraft',
+    console: 'pc',
+    type: 'Strategy',
+    esport: true,
+    imageSrc: ''
+  },
+  {
+    name: 'Call of Duty - Modern Warfare',
+    console: 'pc',
+    type: 'FPS',
+    rating: 3.5,
+    esport: false,
+    imageSrc: ''
+  },
+  {
+    name: 'Call of Duty - Modern Warfare 2',
+    console: 'pc',
+    type: 'FPS',
+    rating: 3.5,
+    esport: false,
+    imageSrc: ''
+  },
+  {
+    name: 'Call of Duty - Modern Warfare 3',
+    console: 'pc',
+    type: 'FPS',
+    rating: 3.5,
+    esport: false,
+    imageSrc: ''
+  },
+  {
+    name: 'Witcher',
+    console: 'pc',
+    type: 'RPG',
+    rating: 4.3,
+    esport: false,
+    imageSrc: ''
+  },
+  {
+    name: 'Need For Speed',
+    expansion: 'Most Wanted',
+    console: 'pc',
+    type: 'Racing',
+    esport: false,
+    imageSrc: ''
+  },
+  {
+    name: 'Need For Speed',
+    expansion: 'Carbon',
+    console: 'pc',
+    type: 'Racing',
+    esport: false,
+    imageSrc: ''
+  },
+  {
+    name: 'Crysis',
+    console: 'pc',
+    type: 'FPS',
+    esport: false,
+    imageSrc: ''
   }
 ];
 
